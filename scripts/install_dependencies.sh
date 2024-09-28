064d92ee0bf34ec265e1775af53a6f98d7d27237
Engr Mejba Ahmed <mejba.13@gmail.com>
2024-09-28 10:17:26 +0600

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# Update and install necessary dependencies
+sudo yum update -y
+sudo yum install -y httpd php php-cli php-mbstring php-xml php-zip unzip git
+
+# Install Composer
 cd /var/www/html
-composer install --no-dev --prefer-dist --optimize-autoloader
+composer install --no-dev --optimize-autoloader
