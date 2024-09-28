d8cf55e08bc7d6cd4d27f58d839692bfa9091cf3
Engr Mejba Ahmed <mejba.13@gmail.com>
2024-09-28 10:39:03 +0600

    Update after_install.sh

--- devops/after_install.sh
+++ devops/after_install.sh
@@ -1,39 +1,42 @@
 #!/bin/bash
 
-# copy system parameter
+# Copy system parameter
 sudo touch /var/www/html/.env
 
-# run composer
-
+# Run Composer install
 sudo composer install --no-dev --ignore-platform-reqs -d /var/www/html
 
+# Install Laravel Telescope (if required)
+cd /var/www/html
+if grep -q TelescopeServiceProvider "config/app.php"; then
+  echo "Telescope is already installed."
+else
+  composer require laravel/telescope --dev
+  php artisan telescope:install
+  php artisan migrate
+fi
 
-# run npm
+# Run npm build
 sudo npm install --prefix /var/www/html
 sudo npm run build --prefix /var/www/html
 
-# user permissions
-sudo chown -R ec2-user:ec2-user /var/www/html
+# Set correct user permissions
+sudo chown -R ec2-user:apache /var/www/html
 
-# selinux permissions
-sudo semanage fcontext -a -t httpd_sys_rw_content_t '/var/www/html(/.*)?'
-sudo semanage fcontext -a -t httpd_log_t '/var/www/html/storage/logs(/.*)?'
-sudo semanage fcontext -a -t httpd_cache_t '/var/www/html/bootstrap/cache(/.*)?'
-sudo restorecon -R -v /var/www/html
-
-# optimize and clear app
-#sudo php /var/www/html/artisan clear-compiled
+# Optimize and clear caches
 sudo php /var/www/html/artisan optimize:clear
 
-# publish horizon
+# Publish Horizon (if needed)
 sudo php /var/www/html/artisan horizon:publish
 
-# supervisor
+# Install Supervisor and restart Horizon
+sudo yum install -y supervisor
+sudo systemctl enable supervisord
+sudo systemctl start supervisord
 sudo supervisorctl update
 sudo supervisorctl restart horizon
 
-# cache config, route, etc
-#sudo php /var/www/html/artisan config:cache
-#sudo php /var/www/html/artisan route:cache
-sudo php /var/www/html/artisan route:trans:cache
+# Cache config, routes, and views
+sudo php /var/www/html/artisan config:cache
+sudo php /var/www/html/artisan route:cache
 sudo php /var/www/html/artisan view:cache
