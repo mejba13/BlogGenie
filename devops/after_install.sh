339e60e5327900c56a9b34d944c2fbedf5157825
Engr Mejba Ahmed <mejba.13@gmail.com>
2024-09-28 11:50:31 +0600

    Update after_install.sh

--- devops/after_install.sh
+++ devops/after_install.sh
@@ -6,15 +6,18 @@ cd /var/www/html
 # Install Composer dependencies
 sudo composer install --no-dev --ignore-platform-reqs
 
-# Install Laravel Telescope (if required)
-if grep -q TelescopeServiceProvider "config/app.php"; then
-  echo "Telescope is already installed."
-else
-  composer require laravel/telescope --dev
-  php artisan telescope:install
-  php artisan migrate
-fi
-
-# Run npm install and build
+# Run npm install and build (if required)
 sudo npm install --prefix /var/www/html
 sudo npm run build --prefix /var/www/html
+
+# Optimize the application (optional but recommended for production)
+php artisan optimize:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Set appropriate permissions for the project directory
+sudo chown -R ec2-user:ec2-user /var/www/html
+
+# Output message indicating deployment success
+echo "Application deployed successfully."
